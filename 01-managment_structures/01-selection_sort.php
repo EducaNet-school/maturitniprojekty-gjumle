114bdd8d1ea4e8a68b2739fc7f7d58f8679045ba
Leoš Gjumija <gjumle@protonmail.com>
2023-01-04 10:07:06 +0100

    Update 01-selection_sort.php

--- 01-managment_structures/01-selection_sort.php
+++ 01-managment_structures/01-selection_sort.php
@@ -3,7 +3,7 @@
 function selection_sort($arr) {
     for ($i = 0; $i < count($arr); $i++) {
         $min = $i;
-        for ($j = $i; count($arr); $j++) {
+        for ($j = $i + 1; $j < count($arr); $j++) {
             if ($arr[$j] < $arr[$min]) {
                 $min = $j;
             }
@@ -18,4 +18,4 @@ function selection_sort($arr) {
 
 // Tests
 $arr = array(6, 5, 8, 2, 16, 42, 15, 10, 20, 4);
-echo selection_sort($arr);
+print_r(selection_sort($arr));
