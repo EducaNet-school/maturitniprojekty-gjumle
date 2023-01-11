3bc43c25b0ecaaf9994f053e20da62893ab60098
Leoš Gjumija <gjumle@protonmail.com>
2023-01-11 10:24:38 +0100

    Update 02-form_control.php

--- 02/02-form_control.php
+++ 02/02-form_control.php
@@ -2,10 +2,29 @@
 
 function name_check($name) {
     // Check if the name has two words
+    $words = array();
+    $words = explode(' ', $name);
+    if (!$words[0]) {
+        return "Missing first name";
+    }
+    elseif (!$words[1]) {
+        return "Missing last name";
+    }
+    else {
+        return "Missing both first and last name";
+    }
 }
 
 function address_check($address) {
-    // Check if the address has at leat 10 char.
+    // Check if the address has at least 10 char.
+    if (strlen($address) < 10) {
+        return "Missing the full address";
+    }
+}
+
+if (isset($_POST['sumbit'])) {
+    echo name_check($_POST['name']);
+    echo address_check($_POST['address']);
 }
 
 ?>
@@ -19,6 +38,6 @@ function address_check($address) {
         <label for='address'>Address:</label>
         <input type='textarea' name='address' id='address'> 
 
-        <input type='submit' value='Submit' name='submit' id='submit'>
+        <input type='submit' value='submit' name='submit' id='submit'>
     </form>
 </html>
