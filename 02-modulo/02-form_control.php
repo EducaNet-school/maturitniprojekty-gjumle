0e1ca9ebe020296a1f8ce92d7782aa3b4b3e376f
Leoš Gjumija <gjumle@protonmail.com>
2023-01-11 09:51:30 +0100

    Update 02-form_control.php

--- 02-modulo/02-form_control.php
+++ 02-modulo/02-form_control.php
@@ -1,2 +1,12 @@
 <?php
 
+?>
+
+<html>
+    <h1>Form control</h1>
+    <form method='post' action=''>
+        <label for='name'>Full name</label>
+        <input type='text' name='name' id='name'>
+        
+        <label for='address'>Address</label>
+        <input type='textarea' name='address' id='address'> 
