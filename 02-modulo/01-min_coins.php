ad4318c72ca37ebbebcfa44e11cc0f0c5c1b5cca
Leoš Gjumija <gjumle@protonmail.com>
2023-01-11 09:40:13 +0100

    Update 01-min_coins.php

--- 02-modulo/01-min_coins.php
+++ 02-modulo/01-min_coins.php
@@ -1,7 +1,7 @@
 <?php
 
 function getMinMoneyCombination($amount) {
-    $denominations = array(1, 2, 5, 10, 20, 50, 100, 200, 500);
+    $denominations = array(1, 2, 5, 10, 20, 50, 100, 200, 500, 1000);
     $minCombination = array();
     $coins = array();
     // Initialize the minimum number of coins needed
