f9999420bfbb40bca7b968e294e5f5a99255a1c7
Leoš Gjumija <gjumle@protonmail.com>
2023-01-11 10:39:10 +0100

    Update 02-form_control.php

--- 02/02-form_control.php
+++ 02/02-form_control.php
@@ -4,14 +4,14 @@ function name_check($name) {
     // Check if the name has two words
     $words = array();
     $words = explode(' ', $name);
-    if (!$words[0]) {
-        return "Missing first name";
+    if (!$words[0] && $words[1]) {
+        return "Missing first name!";
     }
-    elseif (!$words[1]) {
-        return "Missing last name";
+    elseif (!$words[1] && $words[0]) {
+        return "Missing last name!";
     }
     else {
-        return "Missing both first and last name";
+        return "Missing both first and last name!";
     }
 }
 
@@ -22,22 +22,25 @@ function address_check($address) {
     }
 }
 
-if (isset($_POST['sumbit'])) {
+if (isset($_POST['submit'])) {
     echo name_check($_POST['name']);
+    echo '<br>';
     echo address_check($_POST['address']);
 }
 
 ?>
 
 <html>
-    <h1>Form control</h1>
-    <form method='post' action=''>
-        <label for='name'>Full name:</label>
-        <input type='text' name='name' id='name'>
-        
-        <label for='address'>Address:</label>
-        <input type='textarea' name='address' id='address'> 
+    <body>
+        <h1>Form control</h1>
+        <form method='post' action=''>
+            <label for='name'>Full name:</label>
+            <input type='text' name='name' id='name'>
+            
+            <label for='address'>Address:</label>
+            <input type='textarea' name='address' id='address'> 
 
-        <input type='submit' value='submit' name='submit' id='submit'>
-    </form>
+            <input type='submit' value='submit' name='submit' id='submit'>
+        </form>
+    </body>
 </html>
