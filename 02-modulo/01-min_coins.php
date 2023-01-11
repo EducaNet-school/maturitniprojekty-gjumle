fca821d335d7c93ecd35f8e649687be4c848cf66
Leoš Gjumija <gjumle@protonmail.com>
2023-01-11 09:47:36 +0100

    fin

--- 02-modulo/01-min_coins.php
+++ 02-modulo/01-min_coins.php
@@ -1,38 +1,20 @@
 <?php
 
-function getMinMoneyCombination($amount) {
-    $denominations = array(1, 2, 5, 10, 20, 50, 100, 200, 500, 1000);
-    $minCombination = array();
-    $coins = array();
-    // Initialize the minimum number of coins needed
-    // for all amounts to infinity
-    for ($i = 0; $i <= $amount; $i++) {
-        $minCombination[$i] = PHP_INT_MAX;
-    }
-    for ($i = 0; $i <= $amount; $i++) {
-        $coins[$i] = array();
-    }
-    // Base case for amount 0
-    $minCombination[0] = 0;
-    $coins[0][0] = 0;
-    // Compute the minimum number of coins needed
-    // for all amounts from 1 to amount
-    for ($i = 1; $i <= $amount; $i++) {
-        for ($j = 0; $j < count($denominations); $j++) {
-            if ($denominations[$j] <= $i) {
-                $res = $minCombination[$i - $denominations[$j]];
-                // if the current coin denomination + the number of coins needed to make the remaining amount 
-                // is less than the current minimum number of coins needed
-                if ($res != PHP_INT_MAX && $res + 1 < $minCombination[$i]) {
-                    // update the minimum number of coins needed
-                    $minCombination[$i] = $res + 1;
-                    $coins[$i] = $coins[$i - $denominations[$j]];
-                    array_push($coins[$i], $denominations[$j]);
-                }
-            }
+function min_coins($amount) {
+    $coins = array(1, 2, 5, 10, 20, 50, 100, 200, 500);
+    $combination = array();
+    for ($i = count($coins) - 1; $i >= 0; $i--) {
+        while ($amount >= $coins[$i]) {
+            $amount -= $coins[$i];
+            array_push($combination, $coins[$i]);
         }
     }
-    return "Output: " . implode(",", $coins[$amount]);
+    $output = "<ul>";
+    foreach ($combination as $coin) {
+        $output .= "<li>$coin</li>";
+    }
+    $output .= "</ul>";
+    return $output;
 }
 
-echo getMinMoneyCombination(1254);
+echo min_coins(1254);
