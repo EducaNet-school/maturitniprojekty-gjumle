c1b097db69b21d2c2f56c35c90066cddb69c1206
Leoš Gjumija <gjumle@protonmail.com>
2023-01-11 09:55:53 +0100

    Update 02-form_control.php

--- 02/02-form_control.php
+++ 02/02-form_control.php
@@ -1,12 +1,24 @@
 <?php
 
+function name_check($name) {
+    // Check if the name has two words
+}
+
+function address_check($address) {
+    // Check if the address has at leat 10 char.
+}
+
 ?>
 
 <html>
     <h1>Form control</h1>
     <form method='post' action=''>
-        <label for='name'>Full name</label>
+        <label for='name'>Full name:</label>
         <input type='text' name='name' id='name'>
         
-        <label for='address'>Address</label>
+        <label for='address'>Address:</label>
         <input type='textarea' name='address' id='address'> 
+
+        <input type='submit' value='Submit' name='submit' id='submit'>
+    </form>
+</html>
