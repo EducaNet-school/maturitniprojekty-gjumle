0fdd99882e613b385b39c3d651fd5364316505c1
Leoš Gjumija <gjumle@protonmail.com>
2023-01-18 14:25:19 +0100

    Update 01-oop.php

--- 03/01-oop.php
+++ 03/01-oop.php
@@ -16,25 +16,37 @@ class Car {
 
 class Truck extends Car {
     private $load;
-    protected $type = 'truck';
 
     public function __construct($brand, $load) {
-        parrent::__construct($type, $brand);
+        parent::__construct($type = 'truck', $brand);
         $this->load = $load;
+    }
+
+    public function returnTruck() {
+        $ret = parent::returnCar();
+        $ret .= ' Load: ' . $this->load;
+        return $ret;
     }
 }
 
 class Electric extends Car {
     private $batteryCapacity;
-    protected $type = 'electric';
 
     public function __construct($brand, $batteryCapacity) {
-        parrent::__construct($type, $brand);
+        parent::__construct($type = 'electric', $brand);
         $this->batteryCapacity = $batteryCapacity;
+    }
+
+    public function returnElectric() {
+        $ret = parent::returnCar();
+        $ret .= ' Battery capacity: ' . $this->batteryCapacity;
+        return $ret;
     }
 }
 
 $car1 = new Car('personal', 'm-b');
-$car2 = new Truck('ford', 15);
+$car2 = new Truck('ford', 50);
+$car3 = new Electric('tesla', 1500);
 echo $car1->returnCar();
-echo $car2->returnCar();
+echo $car2->returnTruck();
+echo $car3->returnElectric();
