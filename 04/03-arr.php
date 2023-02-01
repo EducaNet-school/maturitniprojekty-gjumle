29817ef42186fef3340cf094a1e81654aa0dc32f
Leoš Gjumija <gjumle@protonmail.com>
2023-02-01 14:34:14 +0100

    Update 03-arr.php

--- 04/03-arr.php
+++ 04/03-arr.php
@@ -1,19 +1,19 @@
 <?php
 
 $arr = [12, 58, 14, 6, 2, [14, 5, [6, 10]]];
+$count = 0;
 
 function arrInArr($arr) {
+    global $count;
     foreach ($arr as $item) {
         if (gettype($item) == 'array') {
-            echo arrInArr($item);
+            arrInArr($item);
         } else {
-            echo $item . " ";
+            echo $item . " + ";
+            $count = $count + $item;
         }
     }
 }
 
-function countArr($arr) {
-    // Count all values in arr
-}
-
 arrInArr($arr);
+echo '= ' . $count;
