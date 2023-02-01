2ecb587605c943782849b910116184d9bba4fa1f
Leoš Gjumija <gjumle@protonmail.com>
2023-02-01 08:38:35 +0100

    Update 02-geometry.php

--- 03/02-geometry.php
+++ 03/02-geometry.php
@@ -1,25 +1,5 @@
 <?php
 
-class Point {
-    protected $x;
-    protected $y;
-
-    public function __construct($x, $y) {
-        $this->x = $x;
-        $this->y = $y;
-    }
-
-    public function returnPoint() {
-        return '[' . $this->x . ';' . $this->y . ']';
-    }
-}
-
-class Triangle extends Point {
-    private $v1;
-    private $v2;
-    private $v3;
-
-    public function __construct($v1, $v2, $v3) {
-        parent::__construct();
-    }
+class Object {
+    
 }
