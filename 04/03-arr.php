974b64579a40ddc834cb9908a6e8d025040751af
Leoš Gjumija <gjumle@protonmail.com>
2023-02-01 09:49:52 +0100

    Create 03-arr.php

---
+++ 04/03-arr.php
@@ -0,0 +1,15 @@
+<?php
+
+$arr = [12, 58, 14, 6, 2, [14, 5, [6, 10]]];
+
+function arrInArr($arr) {
+    foreach ($arr as $item) {
+        if (gettype($item) == 'array') {
+            echo arrInArr($item);
+        } else {
+            echo $item . " ";
+        }
+    }
+}
+
+arrInArr($arr);
