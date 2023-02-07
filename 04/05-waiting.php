899a4ccbfb0bddf88e29fa43ea780e492cfd22d7
Leoš Gjumija <gjumle@protonmail.com>
2023-02-07 21:18:45 +0100

    Update 05-waiting.php

--- 04/05-waiting.php
+++ 04/05-waiting.php
@@ -1,24 +1,26 @@
 <?php
 
-class Wait {
-
-    public function arrival() {
-        $this->in = time();
-    }
-
-}
-
 class Patient {
-    private $name;
+    private $id;
     private $in;
     private $order;
     private $age;
+    private static $no = 0;
 
-    public function __construct($name, $in, $order, $age) {
-        $this->name;
+    public function __construct($in = null, $order = null, $age = null) {
         $this->in;
         $this->order = $order;
         $this->age = $age;
+        self::$no++;
+        $this->id = self::$no;
+    }
+
+    public function arrival() {
+        $this->in = date('Y-m-d h:i:s');
     }
 }
 
+$patient = new Patient ();
+
+$patient->arrival();
+var_dump($patient);
