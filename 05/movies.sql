dd018ba0cc7e58fd66b86586e23d856c42ec0813
Leoš Gjumija <gjumle@protonmail.com>
2023-02-15 09:54:12 +0100

    Update movies.sql

--- 05/movies.sql
+++ 05/movies.sql
@@ -1,4 +1,8 @@
-CREATE DATABASE movies
+CREATE DATABASE movies;
+
+CREATE USER 'movie_manager'@'localhost' IDENTIFIED BY 'mm'; 
+
+GRANT ALL ON movies.* TO 'movie_manager'@'localhost';
 
 CREATE TABLE movie (
     mid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -11,20 +15,20 @@ CREATE TABLE movie (
 CREATE TABLE category (
     cid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL
-)
+);
 
 CREATE TABLE actor (
     aid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     f_name VARCHAR(255) NOT NULL,
     l_name VARCHAR(255) NOT NULL
-)
+);
 
 CREATE TABLE award (
     awid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     description VARCHAR(255) NOT NULL,
     date date NOT NULL,
     place VARCHAR(255) NOT NULL
-)
+);
 
 CREATE TABLE m2c2a2aw (
     m2c2a2awid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -32,8 +36,8 @@ CREATE TABLE m2c2a2aw (
     category_id INT NO NULL,
     actor_id INT NOT NULL,
     award_id INT NOT NULL,
-    FOREIGN KEY movie_id REFERENCES movie(mid),
-    FOREIGN KEY category_id REFERENCES category(cid),
-    FOREIGN KEY actor_id REFERENCES actor(aid),
-    FOREIGN KEY award_id REFERENCES award(awid)
-)
+    CONSTRAINT fk_m2m FOREIGN KEY (movie_id) REFERENCES movie(mid),
+    CONSTRAINT fk_c2c FOREIGN KEY (category_id) REFERENCES category(cid),
+    CONSTRAINT fk_a2a FOREIGN KEY (actor_id) REFERENCES actor(aid),
+    CONSTRAINT fk_aw2aw FOREIGN KEY (award_id) REFERENCES award(awid)
+);
