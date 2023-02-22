9f624c6ba6bde9c1d664f3f926da08a74ca4765c
Leoš Gjumija <gjumle@protonmail.com>
2023-02-22 09:37:41 +0100

    Update movies-data.sql

--- 05/movies-data.sql
+++ 05/movies-data.sql
@@ -39,4 +39,37 @@ VALUES
     ('Best Picture', '1973-04-02', 'Dorothy Chandler Pavilion, Los Angeles'),
     ('Best Actor', '1973-04-02', 'Dorothy Chandler Pavilion, Los Angeles'),
     ('Best Picture', '1995-03-27', 'Shrine Auditorium, Los Angeles'),
-    ('Best Adapted Screenplay', '1995-03-27', 'Shrine
+    ('Best Adapted Screenplay', '1995-03-27', 'Shrine Auditorium, Los Angeles'),
+    ('Best Actor in a Supporting Role', '1995-03-27', 'Shrine Auditorium, Los Angeles'),
+    ('Best Director', '1995-03-27', 'Shrine Auditorium, Los Angeles'),
+    ('Best Picture', '2004-02-29', 'Kodak Theatre, Los Angeles'),
+    ('Best Director', '2004-02-29', 'Kodak Theatre, Los Angeles'),
+    ('Best Adapted Screenplay', '2004-02-29', 'Kodak Theatre, Los Angeles'),
+    ('Best Picture', '2004-02-29', 'Kodak Theatre, Los Angeles'),
+    ('Best Original Screenplay', '2011-02-27', 'Kodak Theatre, Los Angeles'),
+    ('Best Cinematography', '2011-02-27', 'Kodak Theatre, Los Angeles'),
+    ('Best Sound Editing', '2011-02-27', 'Kodak Theatre, Los Angeles');
+
+INSERT INTO m2c2a2aw (movie_id, category_id, actor_id, award_id)
+VALUES
+    (1, 1, 1, 1),
+    (1, 3, 1, 0),
+    (2, 1, 2, 2),
+    (2, 5, 2, 0),
+    (3, 2, 4, 0),
+    (3, 1, 4, 0),
+    (3, 3, 5, 0),
+    (3, 5, 5, 0),
+    (4, 1, 6, 0),
+    (4, 3, 6, 0),
+    (5, 1, 1, 1),
+    (5, 3, 1, 0),
+    (5, 4, 1, 0),
+    (6, 4, 7, 3),
+    (6, 1, 7, 0),
+    (6, 3, 8, 0),
+    (7, 5, 9, 0),
+    (7, 1, 10, 0),
+    (8, 2, 11, 11),
+    (8, 1, 12, 11),
+    (8, 2, 13, 0);
